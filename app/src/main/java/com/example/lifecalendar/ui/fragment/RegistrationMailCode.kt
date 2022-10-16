02de1e99a2da27bb8240ecd2619a91fa39a9d7ab
HantikMine <vadim_dr@hotmail.com>
2022-10-16 16:26:23 +0300

    add registration mail code page && update strings

---
+++ app/src/main/java/com/example/lifecalendar/ui/fragment/RegistrationMailCode.kt
@@ -0,0 +1,19 @@
+package com.example.lifecalendar.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.lifecalendar.R
+
+class RegistrationMailCode : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_registration_mail_code, container, false)
+    }
+}
