3611ec08571039263bfc2fea2f4da470719e98ea
danii <daniilgeroldovich@gmail.com>
2022-10-16 21:57:46 +0500

    Add logic of uploading photo

--- app/src/main/java/com/example/lifecalendar/ui/fragment/RegistrationAvatarFragment.kt
+++ app/src/main/java/com/example/lifecalendar/ui/fragment/RegistrationAvatarFragment.kt
@@ -1,18 +1,165 @@
 package com.example.lifecalendar.ui.fragment
 
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.lifecalendar.R
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import coil.load
+import com.example.lifecalendar.databinding.FragmentRegistrationAvatarBinding
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import com.karumi.dexter.listener.single.PermissionListener
 
 class RegistrationAvatarFragment : Fragment() {
+    
+    private lateinit var binding: FragmentRegistrationAvatarBinding
+    companion object {
+        private const val CAMERA_REQUEST_CODE = 1
+        private const val GALLERY_REQUEST_CODE = 2
+    }
+    
+    
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registration_avatar, container, false)
+    ): View {
+        binding = FragmentRegistrationAvatarBinding.inflate(inflater, container, false)
+        return binding.root
+        
+        binding.addImageFab.setOnClickListener {
+            showPhotoChoosingAlertDialog()
+        }
+    }
+    
+    private fun showPhotoChoosingAlertDialog() {
+        showPhotoChoosingAlertDialog()
+        val pictureDialogItems = arrayOf("Выбрать фото из галереи", "Сделать фото на камеру")
+        AlertDialog.Builder(requireContext())
+            .setTitle("Select Action")
+            .setItems(pictureDialogItems) { _, which ->
+                when (which) {
+                    0 -> galleryCheckPermission()
+                    1 -> cameraCheckPermission()
+                }
+            }.show()
+    }
+    
+    private fun galleryCheckPermission() {
+        Dexter.withContext(requireContext())
+            .withPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+            .withListener(object : PermissionListener {
+                override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
+                    gallery()
+                }
+                
+                override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
+                    Toast.makeText(
+                        requireContext(),
+                        "Зачем  заблочил разрешение на фоточки?",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    showAlertDialogForPermission()
+                }
+                
+                override fun onPermissionRationaleShouldBeShown(
+                    p0: PermissionRequest?,
+                    p1: PermissionToken?
+                ) {
+                    showAlertDialogForPermission()
+                }
+            }).onSameThread().check()
+    }
+    
+    private fun gallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, GALLERY_REQUEST_CODE)
+    }
+    
+    private fun cameraCheckPermission() {
+        Dexter.withContext(requireContext())
+            .withPermissions(
+                android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                android.Manifest.permission.CAMERA
+            ).withListener(
+                object : MultiplePermissionsListener {
+                    override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                        report?.let {
+                            if (report.areAllPermissionsGranted()) {
+                                camera()
+                            }
+                        }
+                    }
+                    
+                    override fun onPermissionRationaleShouldBeShown(
+                        p0: MutableList<PermissionRequest>?,
+                        p1: PermissionToken?
+                    ) {
+                        showAlertDialogForPermission()
+                    }
+                }
+            ).onSameThread().check()
+    }
+    
+    private fun camera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, CAMERA_REQUEST_CODE)
+    }
+    
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                CAMERA_REQUEST_CODE -> {
+                    val bitmap = data?.extras?.get("data") as Bitmap
+                    binding.imagePlace.load(bitmap) /*{
+                        crossfade(true)
+                        crossfade(1000)
+                        transformations(CircleCropTransformation())
+                    }*/
+                }
+                
+                GALLERY_REQUEST_CODE -> {
+                    binding.imagePlace.load(data?.data) /*{
+                        crossfade(true)
+                        crossfade(1000)
+                        transformations(CircleCropTransformation())
+                    }*/
+                }
+            }
+        }
+    }
+    
+    private fun showAlertDialogForPermission() {
+        AlertDialog.Builder(requireContext())
+            .setMessage("Зачем отклонил? Врубай обратно")
+            .setPositiveButton("Go to settings") { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", requireContext().packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: java.lang.Exception) {
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }.show()
     }
 }
