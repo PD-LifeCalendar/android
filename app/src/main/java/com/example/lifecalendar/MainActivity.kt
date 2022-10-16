6941b12a7f599790bca4e0ab633250522b66f50c
danii <daniilgeroldovich@gmail.com>
2022-10-17 00:54:23 +0500

    Add navigation fragment to XML code of activity_main.xml

--- app/src/main/java/com/example/lifecalendar/MainActivity.kt
+++ app/src/main/java/com/example/lifecalendar/MainActivity.kt
@@ -2,17 +2,16 @@ package com.example.lifecalendar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.lifecalendar.ui.fragment.MainFragment
+import com.example.lifecalendar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     
+    private lateinit var binding: ActivityMainBinding
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
