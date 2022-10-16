ca99b808b6306b7b0c153797d4de6312045a4b99
danii <daniilgeroldovich@gmail.com>
2022-10-16 18:19:26 +0500

    Move fragments to fragment package

--- app/src/main/java/com/example/lifecalendar/ui/fragment/RegistrationNameFragment.kt
+++ app/src/main/java/com/example/lifecalendar/ui/fragment/RegistrationNameFragment.kt
@@ -1,10 +1,11 @@
-package com.example.lifecalendar
+package com.example.lifecalendar.ui.fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.lifecalendar.R
 
 class RegistrationNameFragment : Fragment() {
 
