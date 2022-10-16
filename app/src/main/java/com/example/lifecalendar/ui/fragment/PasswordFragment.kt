e589f2e773ec3d7f210e6f35bec2c71be6243e0a
danii <daniilgeroldovich@gmail.com>
2022-10-17 00:55:26 +0500

    Rename Registration Password Fragment

--- app/src/main/java/com/example/lifecalendar/ui/fragment/PasswordFragment.kt
+++
@@ -1,9 +0,0 @@
-package com.example.lifecalendar.ui.fragment
-
-import androidx.fragment.app.Fragment
-
-
-class PasswordFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-
-}
---
+++ app/src/main/java/com/example/lifecalendar/ui/fragment/RegistrationPasswordFragment.kt
@@ -0,0 +1,42 @@
+package com.example.lifecalendar.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.lifecalendar.databinding.FragmentRegistrationPasswordBinding
+
+
+class RegistrationPasswordFragment : Fragment() {
+    
+    private lateinit var binding: FragmentRegistrationPasswordBinding
+    
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentRegistrationPasswordBinding.inflate(inflater, container, false)
+    
+        binding.nextBtn.setOnClickListener {
+            navigateToRegistrationBirthdateFragment()
+        }
+    
+        binding.icBack.setOnClickListener {
+            navigateToRegistrationMailCode()
+        }
+    
+        return binding.root
+    }
+    
+    private fun navigateToRegistrationBirthdateFragment() {
+        val action = RegistrationPasswordFragmentDirections.actionRegistrationPasswordFragmentToRegistrationBirthdateFragment()
+        findNavController().navigate(action)
+    }
+    
+    private fun navigateToRegistrationMailCode() {
+        val action = RegistrationPasswordFragmentDirections.actionRegistrationPasswordFragmentToRegistrationMailCode()
+        findNavController().navigate(action)
+    }
+}
